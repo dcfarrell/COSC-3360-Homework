5771c440577d6132152dc9fcd8f0ecc060b90823
dcfarrell <delaneycfarrell@gmail.com>
2024-08-22 17:11:10 -0500

    Homework 1 Program Files

---
+++ 3360hw1/3360hw1_1631997.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unistd>
+#include <sys/types>
+#include <sys/wait>
+
+using namespace std;
+
+int main(string fileName)
+{
+	ofstream inputFile;
+	inputFile.open(fileName);
+	if (inputFile.is_open())
+	{
+		cout << "Opened " << fileName << ".";
+	}
+	else
+	{
+		cout << "Could not open " << fileName << ".";
+	}
+	inputFile.close();
+	return 0;
+}
